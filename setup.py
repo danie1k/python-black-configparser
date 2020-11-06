ad01df2338e2aa7c1edb2efa618bbc6a647c2bae
Daniel Kuruc <daniel@kuruc.dev>
2020-11-06 23:40:45 +0100

    Rebuild, rename & simplify the tool, bump version to 0.0.5

--- setup.py
+++ setup.py
@@ -1,51 +1,43 @@
 #!/usr/bin/env python
-import os
-
 from setuptools import setup
-
-
-def get_long_description() -> str:
-    path = os.path.join(os.path.dirname(__file__), "README.md")
-    with open(path, encoding="utf8") as fobj:
-        return fobj.read()
+import pathlib
 
 
 setup(
-    name="black_config_files",
-    version="0.0.3",
+    name="black-configparser",
+    version="0.0.5",
+    author="Daniel Kuruc",
+    author_email="daniel@kuruc.dev",
+    license="MIT",
+    url="https://github.com/danie1k/black_configparser",
     description=(
         "Seamless Proxy CLI for black (The uncompromising code formatter) "
         "with support for setup.cfg & tox.ini config files"
     ),
-    long_description=get_long_description(),
+    long_description=(pathlib.Path(__file__).parent / "README.md").read_text("utf8"),
     long_description_content_type="text/markdown",
     keywords=(
         "automation formatter yapf autopep8 pyfmt gofmt rustfmt black white brunette"
     ),
-    author="Daniel Kuruc",
-    author_email="daniel@kuruc.dev",
-    url="https://github.com/danie1k/black_config_files",
-    license="MIT",
-    packages=["black_config_files"],
-    package_dir={"": "src"},
-    package_data={"black_config_files": ["py.typed"]},
+    package_dir={"black_configparser": "src"},
+    packages=["black_configparser"],
+    package_data={"black_configparser": ("py.typed",)},
     python_requires=">=3.6",
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest"],
+    install_requires=("black>=18.6b2", "click>=6.5", "sh>=1.13.0"),
     zip_safe=False,
-    install_requires=["black>=18.6b2", "click>=6.5", "sh>=1.13.0"],
-    test_suite="tests",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Software Development :: Quality Assurance",
     ],
-    entry_points={"console_scripts": ["black=black_config_files:main"]},
 )
---
+++ src/black_configparser.py
@@ -0,0 +1,165 @@
+import collections
+import configparser
+import os
+import sys
+from typing import Deque, Dict, Iterator, List, Optional, TextIO, Tuple, Union
+
+import click
+import sh
+
+__all__ = (
+    "CONFIG_FILES",
+    "CONFIG_SECTIONS",
+    "NO_CONFIG_FLAG",
+    "PWD",
+    "ParamValue",
+    "ParamsType",
+    "_get_options_from_config_files",
+    "_get_options_from_stream",
+    "_make_key",
+    "_make_value",
+    "_open_config_files",
+    "_prepare_argv",
+    "main",
+)
+
+PWD = os.getcwd()
+
+CONFIG_FILES = ("setup.cfg", "tox.ini")
+CONFIG_SECTIONS = ("black", "tools:black")
+NO_CONFIG_FLAG = "no-config-file"
+
+ParamValue = Optional[Tuple[str, ...]]
+ParamsType = Dict[str, ParamValue]
+
+
+def _open_config_files() -> Iterator[Tuple[TextIO, str]]:
+    for file_name in CONFIG_FILES:
+        file_path = os.path.join(PWD, file_name)
+        if os.path.isfile(file_path):
+            with open(file_path, "r") as fobj:
+                yield fobj, file_name
+
+
+def _get_options_from_config_files() -> ParamsType:
+    options: ParamsType = {}
+
+    for file_stream, file_name in _open_config_files():
+        options.update(_get_options_from_stream(file_stream, file_name))
+
+    return options
+
+
+def _get_options_from_stream(  # noqa:C901
+    file_stream: TextIO, file_name: str
+) -> ParamsType:
+    result: ParamsType = {}
+
+    config = configparser.ConfigParser()
+    config.read_file(file_stream)
+
+    for section_name in CONFIG_SECTIONS:
+        if section_name not in config:
+            continue
+
+        click.secho(
+            f"Using [{section_name}] configuration from {file_name}.", fg="blue"
+        )
+
+        for key, value in dict(config[section_name]).items():
+            try:
+                value = config.getboolean(section_name, key)  # type:ignore
+            except ValueError:
+                pass
+            try:
+                value = f"{config.getint(section_name, key)}"
+            except ValueError:
+                pass
+
+            if value is False:  # type:ignore
+                continue
+
+            result[_make_key(key)] = _make_value(value)
+
+    return result
+
+
+def _make_key(value: str) -> str:
+    value = value.replace("_", "-").strip(" -")
+    _prefix = "-" if len(value) == 1 else "--"
+    return f"{_prefix}{value}"
+
+
+def _make_value(value: Union[str, bool]) -> ParamValue:
+    if value is True:
+        return None
+
+    if isinstance(value, str):
+        value = value.strip().split("\n")  # type:ignore
+
+    if not isinstance(value, (list, tuple)):
+        value = [value]  # type:ignore
+
+    return tuple(filter(bool, value))  # type:ignore
+
+
+def _prepare_argv(user_given_argv: List[str]) -> Tuple[str, ...]:  # noqa:C901
+    result: Deque[str] = collections.deque()
+
+    if "--help" in user_given_argv or "--version" in user_given_argv:
+        return tuple(user_given_argv)
+
+    if f"--{NO_CONFIG_FLAG}" in user_given_argv:
+        user_given_argv.remove(f"--{NO_CONFIG_FLAG}")
+        return tuple(user_given_argv)
+
+    if "--config" in user_given_argv:
+        raise RuntimeError(
+            "The --config flag is no longer supported, due to 'black-configparser'.\n"
+            f"Please define options in config file or use --{NO_CONFIG_FLAG} flag."
+        )
+
+    if "--check" in user_given_argv:
+        user_given_argv.remove("--check")
+        result.append("--check")
+
+    if "-c" in user_given_argv or "--code" in user_given_argv:
+        try:
+            _code_arg_index = user_given_argv.index("-c")
+        except ValueError:
+            _code_arg_index = user_given_argv.index("--code")
+
+        result += [
+            user_given_argv.pop(_code_arg_index),
+            user_given_argv.pop(_code_arg_index),
+        ]
+
+    for option, values_list in _get_options_from_config_files().items():
+        if values_list is None:
+            result.append(option)
+            continue
+
+        for value in values_list:
+            result += [option, value]
+
+    for arg in user_given_argv:
+        _maybe_path = arg if os.path.isabs(arg) else os.path.join(PWD, arg)
+        if not (arg[0] != "-" and os.path.exists(_maybe_path)):
+            raise RuntimeError(
+                "Running black command with options is no longer supported, "
+                "due to 'black-configparser'.\n"
+                f"Please define options in config file or use --{NO_CONFIG_FLAG} flag."
+            )
+
+        result.append(arg)
+
+    return tuple(result)
+
+
+def main() -> None:  # pragma:no cover
+    argv = _prepare_argv(sys.argv[1:])
+    sh.python("-m", "black", *argv, _fg=True)  # pylint:disable=no-member
+
+
+if __name__ == "__main__":  # pragma:no cover
+    main()
